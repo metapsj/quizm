29b4738524cab5eb06eab5710da9fd9ba3acda4c
metapsj <patrickjohnson@forty3sixteen.com>
2014-02-26 14:05:50 -0800

    laying the ground for an internal dsl

---
+++ quizm.rb
@@ -0,0 +1,13 @@
+def question(text)
+  puts "Just read a question: #{text}"
+end
+
+def right(text)
+  puts "Just read a correct answer: #{text}"
+end
+
+def wrong(text)
+  puts "Just read an incorrect answer: #{text}"
+end
+
+load 'question.rb'
