12e0e6acf74f78180b041d23cea8edb9936d4a3d
metapsj <patrickjohnson@forty3sixteen.com>
2014-02-26 14:37:20 -0800

    fix requires and misspellings

--- quizm.rb
+++ quizm.rb
@@ -1,17 +1,17 @@
-require 'quiz'
-require 'question'
-require 'answer'
+require_relative 'quiz'
+require_relative 'question'
+require_relative 'answer'
 
 def question(text)
   Quiz.instance.add_question Question.new(text)
 end
 
 def right(text)
-  Quiz.instance.last_question.add_answer Answer.new(test, true)
+  Quiz.instance.last_question.add_answer Answer.new(text, true)
 end
 
 def wrong(text)
-  Quiz.instance.last_question.add_answer Answer.new(text, fase)
+  Quiz.instance.last_question.add_answer Answer.new(text, false)
 end
 
 load 'questions.rb'
