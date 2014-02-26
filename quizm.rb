4cdd33f30cac9cb276dfe6e9a23020015a32d9f1
metapsj <patrickjohnson@forty3sixteen.com>
2014-02-26 14:34:32 -0800

    updated to use quiz, question, answer  classes

--- quizm.rb
+++ quizm.rb
@@ -1,13 +1,19 @@
+require 'quiz'
+require 'question'
+require 'answer'
+
 def question(text)
-  puts "Just read a question: #{text}"
+  Quiz.instance.add_question Question.new(text)
 end
 
 def right(text)
-  puts "Just read a correct answer: #{text}"
+  Quiz.instance.last_question.add_answer Answer.new(test, true)
 end
 
 def wrong(text)
-  puts "Just read an incorrect answer: #{text}"
+  Quiz.instance.last_question.add_answer Answer.new(text, fase)
 end
 
 load 'questions.rb'
+
+Quiz.instance.run_quiz
